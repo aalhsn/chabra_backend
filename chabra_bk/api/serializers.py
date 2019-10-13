eb95dd74169823cfa07ede0bb82522fdd48e3002
bashayer_nouri <bashayer_nouri@hotmail.com>
2019-10-13 15:56:06 +0300

    Authentication

--- chabra_bk/api/serializers.py
+++ chabra_bk/api/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
+
+#Used for Register
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
@@ -15,3 +17,10 @@ class UserCreateSerializer(serializers.ModelSerializer):
         new_user.set_password(password)
         new_user.save()
         return validated_data
+
+
+#Used for Login
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name']
