6a45f1b25e147d022b73f5db71350adfaf8327d0
yousef <yousef.hamadah@gmail.com>
2019-10-16 10:55:07 +0300

    restructure files for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wtshop.wsgi
--- wtshop/settings.py
+++ wtshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#Activate heroku
+django_heroku.settings(locals())
