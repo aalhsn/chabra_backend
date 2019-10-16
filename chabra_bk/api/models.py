7c7eb0063aadf8d3eb45fae59c12fc9a2731220d
bashayer_nouri <bashayer_nouri@hotmail.com>
2019-10-16 10:43:19 +0300

    Modified Profile Model

--- chabra_bk/api/models.py
+++ chabra_bk/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver 
 
 class Product(models.Model):
 	name=models.CharField(max_length=120)
@@ -31,3 +33,12 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return self.user.username
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user= instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()  
